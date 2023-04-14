761d847e227e35e4d1e97dd45c746e1c10e2a980
DURGESH KUMAR <68071906+aditya4-4@users.noreply.github.com>
2023-04-14 11:37:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count =0;
+	for( let key in library){
+		if(library.readingStatus == true){
+			count++;
+		}
+	}
+	return count;
 };
 
 // Do not change the code below
